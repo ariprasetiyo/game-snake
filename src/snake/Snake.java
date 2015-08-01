0d3b1a412cc0f26164a9cc09dfcb8d5786345a7d
ariprasetiyo <prasetiyooo@gmail.com>
2015-08-01 20:17:33 +0700

    first commit

---
+++ src/snake/Snake.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snake;
+
+import javax.swing.JFrame;
+
+
+public class Snake extends JFrame {
+
+    public Snake() {
+
+        add(new Board());
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(320, 340);
+        setLocationRelativeTo(null);
+        setTitle("Snake");
+
+        setResizable(false);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Snake();
+    }
+}
